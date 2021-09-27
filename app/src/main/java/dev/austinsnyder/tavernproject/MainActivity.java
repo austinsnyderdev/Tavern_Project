509da4cb12f394dfea9d73a40f848bc30b2c5068
austinsnyderdev <contact@austinsnyder.dev>
2021-09-27 17:20:34 -0500

    Initial commit

---
+++ app/src/main/java/dev/austinsnyder/tavernproject/MainActivity.java
@@ -0,0 +1,14 @@
+package dev.austinsnyder.tavernproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
