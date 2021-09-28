6e1f4901002c3f39502202788c1d688c2ec1d34f
austinsnyderdev <contact@austinsnyder.dev>
2021-09-27 20:35:34 -0500

    Implemented Firebase, began user registration

--- app/src/main/java/dev/austinsnyder/tavernproject/MainActivity.java
+++ app/src/main/java/dev/austinsnyder/tavernproject/MainActivity.java
@@ -1,8 +1,18 @@
 package dev.austinsnyder.tavernproject;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.ActionCodeSettings;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void accountInformationComplete() {
+        String email = viewToString(R.id.emailLayout);
+        String password = viewToString(R.id.passwordLayout);
+        String displayName = viewToString(R.id.displayNameLayout);
+
+        createAccount(email, password);
+        ActionCodeSettings emailActionCode = new ActionCodeSettings();
+
+    }
+
+    //TODO: Exception cases for null
+    //Takes in an id and returns the string contained in the editText box
+    public String viewToString(int id) {
+        EditText text = findViewById(id);
+        return text.getText().toString();
+    }
+
+    private void createAccount(String email, String password) {
+        // [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+        // [END create_user_with_email]
     }
 }
